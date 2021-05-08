78f3c987ec18ab049d4d45f7a0244ca718634cf7
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-05-08 19:03:44 +0300

    test

--- project/index.ts
+++ project/index.ts
@@ -1,3 +1,8 @@
-export const sum = (a: number, b: number) => a + b;
+export const sum = (a: number, b: number) => {
+    if (a > b) {
+        return b * a;
+    }
+    return a + b;
+};
 
 export const subtr = (a: number, b: number) => a - b;
