30bbeb03dbbf6149f02b079e23faab7d0ffbdf05
Aleksandras Bogdanovas <aleksandras.bogdanovas21@gmail.com>
2023-02-04 14:35:22 +0200

    test

--- tests/sum.test.ts
+++ tests/sum.test.ts
@@ -1,6 +1,9 @@
+import { sum } from '..';
+
 describe('sum', () => {
     it('should sum', () => {
         expect(true).toBe(true);
+        expect(sum(1, 2)).toBe(3);
         console.log('test');
         console.log('alpt');
     });
