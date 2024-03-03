8a59ec9da683d086a459354fd03d5d70e10a503d
sirse <artiom.tretjakovas2@gmail.com>
2024-03-03 17:00:26 +0200

    upd

--- tests/sum.test.ts
+++ tests/sum.test.ts
@@ -2,7 +2,7 @@ import { sum } from '..'
 
 describe("sum", () => {
   it("should sum", () => {
-    expect(true).toBe(true);
+    expect(Math.random() > 0.5).toBe(true);
     expect(sum(1, 2)).toBe(3);
   });
 });
